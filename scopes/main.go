6bcf49743036223119698ff4011488fc7f8c45e9
Ansha1325 <anushasaigollapudi@gmail.com>
2022-01-07 13:19:02 -0600

    4.2

--- scopes/main.go
+++ scopes/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/Anusha1325/assignments/tree/main/scopes/scope"
+	"github.com/Anusha1325/assignments/tree/main/scopes/scope" // importing the package scope
 )
 
 func main() {
-	scope.Parameter()
-	scope.Area()
-	fmt.Println("length: ", scope.Length)
-	//fmt.Println("area: ", area)
+	scope.Parameter() // Calling function Parameter()
+	scope.Area() // Calling function Area()
+
+    /* Calling global variable Length from package scope by importing it */
+	fmt.Println("Length: ", scope.Length)
 }
