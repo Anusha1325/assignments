a87be43614fbe008ea6795ec26c9a6d5e5b223a8
Ansha1325 <anushasaigollapudi@gmail.com>
2022-01-07 21:08:17 -0600

    2

---
+++ datatypes/variables/main.go
@@ -0,0 +1,9 @@
+package main
+
+import ""
+
+func main() {
+	variable.Variables1()
+	variable.Variables2()
+	variable.Variables3()
+}
---
+++ variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+ "anusha/modulesgo/variable"
+)
+
+func main() {
+	variable.Variables1()
+	variable.Variables2()
+	variable.Variables3()
+}
